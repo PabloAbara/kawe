efedb211ab8588fa4abe55fd333d8186b83477be
Pablo Abara <pbabara@uc.cl>
2022-08-26 10:56:36 -0300

    Get or create Roadmap and Checkpoint from respective Models

--- app/controllers/roadmap_controller.rb
+++ app/controllers/roadmap_controller.rb
@@ -2,14 +2,7 @@ class RoadmapController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @roadmap = Roadmap.first()
-    if @roadmap.nil? 
-      @roadmap = Roadmap.new(title: 'El camino para convertirte en dev en 3 meses', project_link: 'https://fintual.notion.site/Paraffin-7eeb003d91614a2cbed2a2e260efde61')
-      @roadmap.save!
-    end
-    @checkpoints = @roadmap.checkpoints()
-    if @checkpoints.empty?
-      p "************************************No hay checkpoints************************************"
-    end
+    @roadmap = Roadmap.get_or_Create()
+    @checkpoints = Checkpoint.get_or_Create(@roadmap)
   end
 end
--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -4,4 +4,17 @@ class Checkpoint < ApplicationRecord
   has_many :completed_checkpoints
   has_many :users, through: :completed_checkpoints  
 
+
+  def self.get_or_Create(roadmap)
+    checkpoint = roadmap.checkpoints.first()
+    if checkpoint.nil? 
+      checkpoint = Checkpoint.new(title: 'Intro to Git', roadmap_id: roadmap.id)
+      checkpoint.save!
+      p "**************** CREANDO UN CHECKPOINT ********************"
+    else
+      p "**************** YA HAY UN CHECKPOINT DE NOMBRE #{checkpoint.title} ********************"
+      p "**************** ACCEDO DESDE CHECKPOINT AL ROADMAP: #{checkpoint.roadmap.title} ********************"
+      return checkpoint
+    end
+  end
 end
