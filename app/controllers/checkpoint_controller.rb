fe2576294e2a1f2d955a1cd50d90dc94200535ee
Pablo Abara <pbabara@uc.cl>
2022-08-29 15:54:48 -0300

    feat: create first resource evaluation without conditions

--- app/controllers/checkpoint_controller.rb
+++ app/controllers/checkpoint_controller.rb
@@ -3,7 +3,9 @@ class CheckpointController < ApplicationController
     @checkpoint = Roadmap.first().checkpoints[0]
   end
 
-  def resource_path
+  def resource
+     ResourceRating.create!(:user_id => current_user.id, :resource_id => params[:resource][:resource_id].to_i, :rating => params[:resource][:rating].to_i)
+     p "************************************************************Se ha creado un resource rating!! ************************************************************"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'checkpoint/show'
   devise_for :users
-  root 'roadmap#show'
+  root 'roadmap#index'
+  patch 'resource' , to: 'checkpoint#resource'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
