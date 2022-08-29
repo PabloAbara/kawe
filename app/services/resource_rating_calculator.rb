dcfa929a82cb49382a4e35d956f48a9a694a56be
Pablo Abara <pbabara@uc.cl>
2022-08-29 17:13:52 -0300

    feat: create calculator service

---
+++ app/services/resource_rating_calculator.rb
@@ -0,0 +1,9 @@
+class ResourceRatingCalculator < ApplicationController
+
+  def self.calculate_rating(resources)
+
+    #ResourceRating.group(:resource_id).average(:rating)[resource_id]
+    p "****************************************************************HOLA****************************************************************"
+  end
+
+end
