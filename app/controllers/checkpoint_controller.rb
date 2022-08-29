ff9410627bc838be610b9e34ab8342cea143dc48
Pablo Abara <pbabara@uc.cl>
2022-08-29 16:48:44 -0300

    refactor: change method and route name for rating resource

--- app/controllers/checkpoint_controller.rb
+++ app/controllers/checkpoint_controller.rb
@@ -1,11 +1,25 @@
 class CheckpointController < ApplicationController
+  before_action :set_checkpoint, only: [:show, :rate_resource]
+
   def show
-    @checkpoint = Roadmap.first().checkpoints[0]
   end
 
-  def resource
-     ResourceRating.create!(:user_id => current_user.id, :resource_id => params[:resource][:resource_id].to_i, :rating => params[:resource][:rating].to_i)
-     p "************************************************************Se ha creado un resource rating!! ************************************************************"
+  def rate_resource
+     ResourceRating.create!(
+      user_id: current_user.id, 
+      resource_id: resource_params[:resource_id].to_i, 
+      rating: resource_params[:rating].to_i
+    )
+     
+     redirect_to checkpoint_show_url(@checkpoint)
+  end
+
+  def resource_params
+    params.require(:resource).permit(:resource_id, :rating)
+  end
+
+  def set_checkpoint
+    @checkpoint = Roadmap.first.checkpoints.first
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'checkpoint/show'
   devise_for :users
   root 'roadmap#index'
-  patch 'resource' , to: 'checkpoint#resource'
+  post 'rate_resource' , to: 'checkpoint#rate_resource'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
