5d18aee091ae1b102d7efacb7ffe3873c26e74d1
Juan Santos <jjsantosochoa@outlook.com>
2022-08-30 15:40:52 -0300

    feat: check resource rating not null Check if resource has ratings before calculate average

--- app/services/resource_rating_calculator.rb
+++ app/services/resource_rating_calculator.rb
@@ -1,14 +1,8 @@
 class ResourceRatingCalculator
 
   def self.calculate_resource_rating(resource)
-    #TODO: refactor, too many lines
     ratings = ResourceRating.where(resource_id: resource.id)
-    unless ratings.empty?
-      output = ratings.average(:rating).round(2)
-    else
-      output = "--"
-    end
-    output
+    if ratings.any? then ratings.average(:rating).round(2) else nil end
   end
 
 end
