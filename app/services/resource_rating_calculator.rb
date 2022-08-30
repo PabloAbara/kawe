5aadc533bc899c6960d988c7c20996c25a9eedf1
Pablo Abara <pbabara@uc.cl>
2022-08-30 09:16:38 -0300

    feat: add rating calculation through service

--- app/services/resource_rating_calculator.rb
+++ app/services/resource_rating_calculator.rb
@@ -1,21 +1,7 @@
-class ResourceRatingCalculator < ApplicationController
+class ResourceRatingCalculator
 
-  #recibe un listado de resources y retorna un array con los pares [resource,rating]
-  def self.calculate_resources_rating(resources)
-    resource_with_rating = []
-    resources.each do |resource|
-      resource_with_rating.push([resource,calculate_resource_rating(resource)])
-    end
-
-    p "****************************************************************HOLA****************************************************************"
-  end
-
-  def calculate_resource_rating(resource)
-    #ToDo: calcular el average para cada resource
-    
-    #calculation_basis = .
-    #zombies = ResourceRating.filter_by(:id) if params[:weapons_ids].present?
-
+  def self.calculate_resource_rating(resource)
+    ResourceRating.where(resource_id: resource.id).average(:rating)
   end
 
 end
