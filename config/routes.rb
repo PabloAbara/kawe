6e799eb4fbb0d6bd7b63addbb87b4729ef14d664
Pablo Abara <pbabara@uc.cl>
2022-08-31 16:27:50 -0300

    feat: add clickable row on checkpoint name

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
