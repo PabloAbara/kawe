4d843fb03b542379cfdf574d584d95a7d6e9b1ed
Pablo Abara <pbabara@uc.cl>
2022-08-31 18:33:00 -0300

    test: add constraint of uniqueness for rating_resources and completed_checkpoints models

--- app/models/completed_checkpoint.rb
+++ app/models/completed_checkpoint.rb
@@ -1,4 +1,7 @@
 class CompletedCheckpoint < ApplicationRecord
   belongs_to :user
   belongs_to :checkpoint
+
+  validates :checkpoint_id, uniqueness: { scope: :user_id, message: "El usuario ya ha completado este Checkpoint." }
+
 end
