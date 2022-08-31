02ebbfbc8df7f151a21d2f0b32375dad7aff8da2
Pablo Abara <pbabara@uc.cl>
2022-08-31 18:45:05 -0300

    test: add model and view validation for resource creation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,7 @@
 class Resource < ApplicationRecord
   belongs_to :checkpoint
   has_many :resource_ratings, dependent: :destroy
+
+  validates :link,
+            format: { with: %r{\A(https?://)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*/?\Z}i }
 end
