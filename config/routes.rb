91b84ac859313ff2d0651b0d9c0e7435f89257ff
Juan Santos <jjsantosochoa@outlook.com>
2022-08-31 16:11:56 -0300

    feat: add name field to sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   root "roadmaps#show"
 
   resources :roadmaps, only: %i[index show] do
