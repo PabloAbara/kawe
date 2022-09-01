703478c214a12917b829ab7496d0a29e823bc90c
Pablo Abara <pbabara@uc.cl>
2022-09-01 09:42:27 -0300

    fix: link validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,4 @@
 class Resource < ApplicationRecord
   belongs_to :checkpoint
   has_many :resource_ratings, dependent: :destroy
-
-  validates :link, format: { with: %r{\A(https?://)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*/?\Z}i }
 end
