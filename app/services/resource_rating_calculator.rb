b8844638ce2621ba95fc642bfb0521c1ddac7d1b
Pablo Abara <pbabara@uc.cl>
2022-09-07 15:59:45 -0300

    refactor: implement resource_rating_calculator service on show checkpoint view

--- app/services/resource_rating_calculator.rb
+++ app/services/resource_rating_calculator.rb
@@ -3,20 +3,22 @@ class ResourceRatingCalculator
   def self.get_resources_with_average(checkpoint)
     result = nil
     unless checkpoint.nil?
-      result = {}
+      result = []
       checkpoint.resources.each do |resource|
-        result.store(resource.id, {
-          title: resource.title, 
+        result.push({
+          id: resource.id,          
+          title: resource.title,
           link: resource.link,
           average_rating: calculate_resource_rating(resource) })
       end
     end
-    puts result
     if result
-      result = result.sort_by { |k, v| v[:average_rating] }
+      result = result.sort_by { |v| v[:average_rating] }.reverse
     end
     result
   end
+
+  private
 
   def self.calculate_resource_rating(resource)
     ratings = ResourceRating.where(resource_id: resource.id)
