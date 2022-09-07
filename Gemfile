3347506d32b3ac2694e03b80c996cd41ef67608d
Pablo Abara <pbabara@uc.cl>
2022-09-07 16:35:51 -0300

    fix: rubocop corrections

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "devise", "~> 4.8", ">= 4.8.1"
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
+  gem "annotate"
   gem "debug", platforms: %i[mri mingw x64_mingw]
   gem "dotenv-rails"
   gem "factory_bot_rails"
@@ -58,7 +59,6 @@ group :development, :test do
   gem "rubocop-performance", require: false
   gem "rubocop-rails", require: false
   gem "rubocop-rspec", require: false
-  gem 'annotate'
 end
 
 group :development do
--- app/services/resource_rating_calculator.rb
+++ app/services/resource_rating_calculator.rb
@@ -1,27 +1,25 @@
 class ResourceRatingCalculator
-
   def self.get_resources_with_average(checkpoint)
     result = nil
     unless checkpoint.nil?
       result = []
       checkpoint.resources.each do |resource|
         result.push({
-          id: resource.id,          
-          title: resource.title,
-          link: resource.link,
-          average_rating: calculate_resource_rating(resource) })
+                      id: resource.id,
+                      title: resource.title,
+                      link: resource.link,
+                      average_rating: calculate_resource_rating(resource)
+                    })
       end
     end
-    if result
-      result = result.sort_by { |v| v[:average_rating] }.reverse
-    end
+    result = result.sort_by { |v| v[:average_rating] }.reverse if result
     result
   end
 
-  private
-
-  def self.calculate_resource_rating(resource)
-    ratings = ResourceRating.where(resource_id: resource.id)
-    ratings.any? ? ratings.average(:rating).round(2) : 0.0
+  class << self
+    def calculate_resource_rating(resource)
+      ratings = ResourceRating.where(resource_id: resource.id)
+      ratings.any? ? ratings.average(:rating).round(2) : 0.0
+    end
   end
 end
