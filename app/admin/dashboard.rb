94a09359120e1bce05d085201dcd47df71f9b849
Isa Vega <igvega02@gmail.com>
2022-09-08 10:45:36 -0300

    feat: add checkpoints to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,20 +5,18 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container" do
-      h2 "Seguimiento del Roadmap X"
+      h2 "Seguimiento del Roadmap: #{Roadmap.first.title}"
     end
-      # @roadmap.checkpoints.each do |checkpoint|
+    Roadmap.first.checkpoints.each do |checkpoint|
       div class: "wrapper" do
         div class: "skill" do
-          # h3 checkpoint.title
+          h3 checkpoint.title
           h3 "Checkpoint 1"
           div class: "skill-bar skill1" do
             span class: "skill-count1" "95%"
           end
         end
       end
-    # end
+    end
   end
-
-
 end
