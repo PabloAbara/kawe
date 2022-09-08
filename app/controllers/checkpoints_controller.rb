f8ea9f707fac4a6fe74acf604e743e5772540d6c
Pablo Abara <pbabara@uc.cl>
2022-09-08 17:14:11 -0300

    refactor: shorten validations to private methods on controller

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -8,27 +8,27 @@ class CheckpointsController < ApplicationController
   end
 
   def rate_resource
-    if resource_params.nil? || resource_params[:resource_id].nil? || resource_params[:rating].nil?
+    if rate_resource_params.nil?
       redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint),
                   alert: "Error: Recurso no evaluado"
     else
       new_resource_rating = ResourceRating.create(
         user_id: current_user.id,
-        resource_id: resource_params[:resource_id].to_i,
-        rating: resource_params[:rating].to_i
+        resource_id: rate_resource_params[:resource_id].to_i,
+        rating: rate_resource_params[:rating].to_i
       )
-      alert_new_resource_rating = new_resource_rating.errors.any? ? new_resource_rating.errors.full_messages.join(";") : ""
-      if alert_new_resource_rating.empty?
+      if new_resource_rating.errors.any?
+        alert = new_resource_rating.errors.full_messages.join(";")
+        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert
+      else
         redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint),
                     notice: "Se ha evaluado un recurso!"
-      else
-        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert_new_resource_rating
       end
     end
   end
 
   def create_resource
-    if new_resource_params.nil? || new_resource_params[:title].empty? || new_resource_params[:link].empty?
+    if new_resource_params.nil?
       redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: "Error: Recurso no creado"
     else
       new_resource = Resource.create(
@@ -36,12 +36,12 @@ class CheckpointsController < ApplicationController
         title: new_resource_params[:title],
         link: new_resource_params[:link]
       )
-      alert_new_resource = new_resource.errors.any? ? new_resource.errors.full_messages.join(";") : ""
-      if alert_new_resource.empty?
+      if new_resource.errors.any?
+        alert = new_resource.errors.full_messages.join(";")
+        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert
+      else
         redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint),
                     notice: "Se ha creado un nuevo recurso!"
-      else
-        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert_new_resource
       end
     end
   end
@@ -50,12 +50,12 @@ class CheckpointsController < ApplicationController
     if params[:resource_id].present? && @checkpoint.resources.find_by(id: params[:resource_id])
       resource = @checkpoint.resources.find_by(id: params[:resource_id])
       deleted_resource = resource.destroy!
-      alert_deleted_resource = deleted_resource.errors.any? ? deleted_resource.errors.full_messages.join(";") : ""
-      if alert_deleted_resource.empty?
+      if deleted_resource.errors.any?
+        alert = deleted_resource.errors.full_messages.join(";")
+        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert
+      else
         redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint),
                     notice: "El recurso ha sido eliminado."
-      else
-        redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint), alert: alert_deleted_resource
       end
     else
       redirect_to roadmap_checkpoint_path(@roadmap, @checkpoint),
@@ -77,15 +77,11 @@ class CheckpointsController < ApplicationController
     params.require(:roadmap_id)
   end
 
-  def resource_params
+  def rate_resource_params
     params.require(:resource).permit(:resource_id, :rating)
   end
 
   def new_resource_params
-    if params[:resource].present? && params[:title].present? && params[:link].present?
-      params.require(:resource).permit(:title, :link)
-    else
-      nil
-    end
+    params.require(:resource).permit(:title, :link) if params[:resource]
   end
 end
