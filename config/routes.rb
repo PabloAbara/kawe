dd9033facf1834b4b7701ceb6e9e2e02692ccf73
Pablo Abara <pbabara@uc.cl>
2022-09-09 12:34:09 -0300

    fix: change root when user unathenticated

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: "users/registrations" }
-  root "roadmaps#show"
+  devise_scope :user do
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  root "roadmaps#index"
 
   resources :roadmaps, only: %i[index show] do
     post "complete_checkpoint"
