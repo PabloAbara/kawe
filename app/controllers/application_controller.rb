dafc8452c4136d24c4d7cd49fee0702f49c7362a
Pablo Abara <pbabara@uc.cl>
2022-09-09 16:18:57 -0300

    fix: rubocop action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     redirect_to admin_organizations_path, alert: exception.message
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 
