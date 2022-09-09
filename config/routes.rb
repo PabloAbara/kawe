d186e8f3b6357577b56d2bd9c050512015e59c77
Pablo Abara <pbabara@uc.cl>
2022-09-09 16:57:57 -0300

    fix: rubocop action

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: "users/registrations" }
-  get 'home/index'
-  root 'home#index'
+  get "home/index"
+  root "home#index"
 
   resources :roadmaps, only: %i[index show] do
     post "complete_checkpoint"
