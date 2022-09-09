1b7e1bfd0e8608195386f2770c8c79eb63d0cd28
Pablo Abara <pbabara@uc.cl>
2022-09-09 14:36:21 -0300

    style: add navbar alignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: "users/registrations" }
-  devise_scope :user do
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
   root "roadmaps#index"
 
   resources :roadmaps, only: %i[index show] do
