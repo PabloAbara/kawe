e1b44579a950ef4527d2583f20a8f40c771395ef
Pablo Abara <pbabara@uc.cl>
2022-09-09 09:54:06 -0300

    test: add tests with many resources and resource_ratings

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,7 +14,7 @@
 #
 FactoryBot.define do
   factory :user do
-    email { "test1@fin.cl" }
+    sequence(:email) { |n| "test#{n}@fin.cl" }
     name { "Test2" }
     password { "123456" }
     password_confirmation { "123456" }
--- spec/services/resource_rating_calculator_spec.rb
+++ spec/services/resource_rating_calculator_spec.rb
@@ -30,8 +30,38 @@ RSpec.describe ResourceRatingCalculator do
     end
 
     context "with many resources and ratings" do
-      it "returns every average"
-      it "sorts correctly"
+      let!(:user_two) { create(:user) }
+      let!(:resource_one) { create(:resource, checkpoint: checkpoint) }
+
+      let!(:user_rating_resource_one) do
+        create(:resource_rating, rating: 2, user: user, resource: resource_one)
+      end
+      let!(:user_two_rating_resource_one) do
+        create(:resource_rating, rating: 5, user: user_two, resource: resource_one)
+      end
+
+      let!(:resource_two) { create(:resource, checkpoint: checkpoint) }
+      let!(:user_rating_resource_two) do
+        create(:resource_rating, rating: 3, user: user, resource: resource_two)
+      end
+      let!(:user_two_rating_resource_two) do
+        create(:resource_rating, rating: 5, user: user_two, resource: resource_two)
+      end
+
+      it "returns every average" do
+        resources_rated = described_class.get_resources_with_average(checkpoint)
+
+        expect(resources_rated.pluck(:average_rating)).to include(3.5)
+        expect(resources_rated.pluck(:average_rating)).to include(4)
+      end
+
+      it "sorts correctly" do
+        resources_rated = described_class.get_resources_with_average(checkpoint)
+
+        expect(
+          resources_rated.pluck(:average_rating)
+        ).to eq(resources_rated.pluck(:average_rating).sort.reverse)
+      end
     end
   end
 end
